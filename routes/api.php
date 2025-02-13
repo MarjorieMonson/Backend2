976f7e18a7fb20e8b96224474e34341ef02db5bf
MarjorieMonson <20235962@esen.edu.sv>
2025-02-13 13:01:51 -0600

    Metodos

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
 
 Route::group(['prefix' => 'v1'], function () {
     Route::post('/users', [UserController::class, 'store']);
     Route::get('/users', [UserController::class, 'index']);
+    Route::put('/users/{user}',[UserController::class,'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
